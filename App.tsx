6eaba12fae11d88a87001f55999b18b46e134ed9
lucas <lucas.reynacordoba22@gmail.com>
2022-06-09 23:11:15 -0300

    Adds tab navigation

--- App.tsx
+++ App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { NativeStackNavigator, BottomTabNavigator } from './src/utils';
+import { /* NativeStackNavigator, */ BottomTabNavigator } from './src/utils';
+import AppProvider from './src/utils/app_context';
 
 const App = () => {
+
     return (
         <SafeAreaProvider>
-            <NavigationContainer>
-                <StatusBar
-                    style="auto"
-                />
-                <NativeStackNavigator />
-                <BottomTabNavigator />
-            </NavigationContainer>
+            <AppProvider>
+                <NavigationContainer>
+                    <StatusBar
+                        style="auto"
+                    />
+                    {/* <NativeStackNavigator /> */}
+                    <BottomTabNavigator />
+                </NavigationContainer>
+            </AppProvider>
         </SafeAreaProvider>
     );
 };
--- src/utils/botton-tabs.tsx
+++ src/utils/botton-tabs.tsx
@@ -1,20 +1,121 @@
 import React from 'react';
+
+// Screens
+import {
+    HomeScreen,
+    AssetsScreen,
+    TradesScreen,
+    NotificationsScreen,
+    ProfileScreen
+} from '../screens';
+
+// Bottom Tabs
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { LandingScreen, HomeScreen } from '../screens';
+// Icons
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: '#e98f1e',
+                tabBarInactiveTintColor: '#888',
+                headerShown: false,
+                tabBarLabel: () => false,
+                tabBarStyle: {
+                    height: 60,
+                    backgroundColor: '#fff',
+                    position: 'absolute',
+                    bottom: 35,
+                    marginHorizontal: 20,
+                    borderRadius: 10,
+                    shadowColor: '#222',
+                    shadowOpacity: .06,
+                    shadowOffset: {
+                        width: 10,
+                        height: 10
+                    },
+                    paddingHorizontal: 20
+                },
+                tabBarBadgeStyle: {
+                    backgroundColor: '#e98f1e',
+                    color: '#fff'
+                }
+            }}
+        >
             <Tab.Screen
-                name="Landing"
-                component={LandingScreen}
+                name="Dashboard"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <FontAwesome5
+                            name="home"
+                            focused={focused}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
             <Tab.Screen
-                name="Home"
-                component={HomeScreen}
+                name="Assets"
+                component={AssetsScreen}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <FontAwesome
+                            name="dollar"
+                            focused={focused}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Trades"
+                component={TradesScreen}
+                options={{
+                    tabBarIcon: ({ size, color, focused }) => (
+                        <FontAwesome
+                            name="exchange"
+                            focused={focused}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Notifications"
+                component={NotificationsScreen}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <FontAwesome5
+                            name="bell"
+                            focused={focused}
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                    tabBarBadge: 3
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <FontAwesome5
+                            name="user"
+                            focused={focused}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
         </Tab.Navigator>
     );
