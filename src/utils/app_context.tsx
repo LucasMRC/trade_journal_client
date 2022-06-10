2ba0f9702b416720ae579a6dcc193daa29d4c3a5
lucas <lucas.reynacordoba22@gmail.com>
2022-06-09 23:10:34 -0300

    Creates app global state

---
+++ src/utils/app_context.tsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState } from 'react';
+
+interface ProviderProps {
+    children: React.ReactNode;
+}
+
+const AppContext = createContext<{
+    notifications: number;
+    setNotifications: React.Dispatch<React.SetStateAction<number>>;
+}>({
+    notifications: 0,
+    setNotifications: () => null
+});
+
+const AppProvider: React.FC<ProviderProps> = ({ children }: ProviderProps) => {
+    const [ notifications, setNotifications ] = useState(0);
+
+    return (
+        <AppContext.Provider
+            value={{ notifications, setNotifications }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+    if (!context) {
+        throw new Error('useAppContext must be used within an AppProvider');
+    }
+
+    return context;
+};
+
+export default AppProvider;
