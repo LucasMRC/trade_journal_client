83b81b109c4e221eb0494f30e2bab6ed3f4af7c7
lucas <lucas.reynacordoba22@gmail.com>
2022-06-17 14:53:25 -0300

    Fixes ts paths aliases 👍

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+	api.cache(true);
+	return {
+    	presets: ['babel-preset-expo'],
+    	plugins: [
+      		[
+				'module-resolver',
+				{
+					alias: {
+						'@': './src',
+						'@assets': './assets',
+						'@screens': './src/screens',
+						'@redux': './src/redux',
+						'@api': './src/api',
+						'@utils': './src/utils',
+						'@components': './src/components'
+					}
+				}
+			]
+		]
+	};
 };
